ad1b03f27803875406639d6025514645302f8802
fshatskiy <f.shatskiy@students.ephec.be>
2021-01-21 23:08:42 +0100

    Add project files.

---
+++ Controllers/FacturesController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RhezomFacAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RhezomFacAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturesController : ControllerBase
+    {
+
+        [HttpGet("GetFactures")]
+        public IActionResult GetFactures()
+        {
+            //appeler la bd
+            // var produitsList = porduitsRepository.GetAll();
+
+            var factures = new List<FactureModel>
+            {
+                new FactureModel
+                {
+                    //try data static
+                    Id = 1,
+                    DateEcheance = DateTime.Now,
+                    Produits = new List<ProduitModel>
+                    {
+                        new ProduitModel
+                        {
+                            //try data static
+                            Id = 1,
+                            Description = "Produit",
+                            EstActif=true,
+                            Nom = "Other product",
+                            PrixHT = 21,
+                            QtiteRest = 1,
+                            TVA = 25.0f
+                        },
+                        new ProduitModel
+                        {
+                            //try data static
+                            Id = 2,
+                            Description = "Produit2",
+                            EstActif=true,
+                            Nom = "Prod2",
+                            PrixHT = 21,
+                            QtiteRest = 1,
+                            TVA = 25.0f
+                        }
+                    }
+                },
+                new FactureModel
+                {
+                    //try data static
+                    Id = 2,
+                    DateEcheance = DateTime.Now,
+                    Produits = new List<ProduitModel>
+                    {
+                        new ProduitModel
+                        {
+                            //try data static
+                            Id = 3,
+                            Description = "Produit",
+                            EstActif=true,
+                            Nom = "Other product",
+                            PrixHT = 21,
+                            QtiteRest = 1,
+                            TVA = 25.0f
+                        }
+                    }
+                }
+            };
+
+
+            return Ok(factures);
+        }
+
+        [HttpPost("SaveFacture")]
+
+        public IActionResult SaveFacture([FromBody] FactureModel factureModel)
+        {
+            bool success = false;
+            // CAll DB Save Model; Commit;
+            return Ok(success);
+        }
+
+    }
+}
---
+++ Controllers/ProduitsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RhezomFacAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RhezomFacAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProduitsController : ControllerBase
+    {
+
+        [HttpGet("GetProduits")]
+        public IActionResult GetProduits()
+        {
+            //appeler la bd
+            // var produitsList = porduitsRepository.GetAll();
+
+            var Produits = new List<ProduitModel>
+            {
+                new ProduitModel
+                {
+                    //try data static
+                    Id = 1,
+                    Description = "Produit",
+                    EstActif=true,
+                    Nom = "Other product",
+                    PrixHT = 21,
+                    QtiteRest = 1,
+                    TVA = 25.0f
+                },
+                new ProduitModel
+                {
+                    //try data static
+                    Id = 2,
+                    Description = "Produit2",
+                    EstActif=true,
+                    Nom = "Prod2",
+                    PrixHT = 21,
+                    QtiteRest = 1,
+                    TVA = 25.0f
+                }
+            };
+
+
+            return Ok(Produits);
+        }
+    }
+}
